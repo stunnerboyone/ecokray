d6984aa3c9ef6f529fc6a86c00b751496a226223
unknown <stunnerboy691@gmail.com>
2025-06-13 12:53:52 +0300

    noveposta fix

---
+++ check-ioncube.php
@@ -0,0 +1,8 @@
+<?php
+echo 'ionCube: ';
+echo extension_loaded('ionCube Loader') ? '✅ ACTIVE' : '❌ NOT ACTIVE';
+echo "\n";
+
+echo 'Function _il_exec(): ';
+echo function_exists('_il_exec') ? '✅ EXISTS' : '❌ NOT FOUND';
+echo "\n";
