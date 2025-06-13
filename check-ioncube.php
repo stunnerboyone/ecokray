b772c4a942f3e6bead26b7d1208076b9d768a27c
unknown <stunnerboy691@gmail.com>
2025-06-13 13:07:17 +0300

    check ioncube

--- check-ioncube.php
+++ check-ioncube.php
@@ -6,3 +6,6 @@ echo "\n";
 echo 'Function _il_exec(): ';
 echo function_exists('_il_exec') ? '✅ EXISTS' : '❌ NOT FOUND';
 echo "\n";
+
+$all = get_defined_functions();
+echo in_array('_il_exec', $all['internal']) ? "_il_exec FOUND ✅\n" : "_il_exec NOT FOUND ❌\n";
